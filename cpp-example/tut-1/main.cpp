22211f2c93bcd91d7e444d90f2f3c710fd99b25b
goolyuyi <goolyuyi@hotmail.com>
2022-09-14 22:30:35 +0800

    daily

---
+++ cpp-example/tut-1/main.cpp
@@ -0,0 +1,23 @@
+//
+// Created by 于一 on 2022/9/2.
+//
+
+#include <iostream>
+#include <pxr/usd/usd/stage.h>
+#include <pxr/usd/usdGeom/xform.h>
+#include <pxr/usd/usdGeom/sphere.h>
+
+int main() {
+    using namespace pxr;
+    using namespace std;
+    const auto stage = UsdStage::CreateNew("HelloWorld.usda");
+    auto xformPrim = UsdGeomXform::Define(stage, SdfPath("/hello"));
+    auto spherePrim = UsdGeomSphere::Define(stage, SdfPath("/hello/world"));
+
+    auto genericPrim = spherePrim.GetPrim();
+    for (auto i: genericPrim.GetAttributes()) {
+        cout << i.GetDescription() << endl;
+    }
+
+    stage->GetRootLayer()->Save();
+}
--- hello-usd.py
+++ hello-usd.py
@@ -1,5 +1,10 @@
 from pxr import Usd, UsdGeom
+
 stage = Usd.Stage.CreateNew('HelloWorld.usda')
 xformPrim = UsdGeom.Xform.Define(stage, '/hello')
 spherePrim = UsdGeom.Sphere.Define(stage, '/hello/world')
-stage.GetRootLayer().Save() #comment
+stage.GetRootLayer().Save()  # comment
+
+
+def aaaa():
+    return 0
