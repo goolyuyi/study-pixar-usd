d292254495cfd55ab02ea5458f5f8e49d531fdfd
goolyuyi <goolyuyi@hotmail.com>
2022-09-27 01:01:08 +0800

    xxx

--- examples-py/hello-usd.py
+++ examples-py/hello-usd.py
@@ -3,4 +3,4 @@ from pxr import Usd, UsdGeom
 stage = Usd.Stage.CreateNew('HelloWorld.usda')
 xformPrim = UsdGeom.Xform.Define(stage, '/hello')
 spherePrim = UsdGeom.Sphere.Define(stage, '/hello/world')
-stage.GetRootLayer().Save()  # comment
+stage.GetRootLayer().Save()
